9eb6f5ed2b1b848b423e25a2c1a2a54fb5820152
GladmanWorks <gladmanworks@gmail.com>
2024-02-19 10:49:34 +0200

    fixed logo & colors

---
+++ public/js/sections-Disclaimer-4bb917c2183c07fc5fb6.js
@@ -0,0 +1,1 @@
+"use strict";(self.__LOADABLE_LOADED_CHUNKS__=self.__LOADABLE_LOADED_CHUNKS__||[]).push([[8360],{70213:function(e,t,r){r.r(t),r.d(t,{default:function(){return c}});var _=r(67294),a=r(69931),l=r.n(a),n=r(8545),s=r(38602),u=r(84894);var c=e=>{let{text:t}=e;return _.createElement(n.Z,{extraClassName:l()("Disclaimer-disclaimer--1O21g",u.VJ)},_.createElement(s.default,{body:t}))}}}]);
